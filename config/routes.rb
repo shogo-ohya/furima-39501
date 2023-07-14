4e1a3f669e1ac1deb796948af01826e6fe062241
ぴえん <136320612+shogo-ohya@users.noreply.github.com>
2023-07-14 19:41:53 +0900

    ログイン機能途中（一時コミット）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :views
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
 
   resources :users
 
