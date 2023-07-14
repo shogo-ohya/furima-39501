4168899d09ff43d4112c166aa252064bb686cb73
ぴえん <136320612+shogo-ohya@users.noreply.github.com>
2023-07-14 12:42:55 +0900

    新規登録画面のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+          
+         validates :nickname,presence: true
+         validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
+         validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
+         validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: "は全角文字で入力してください" }
+         validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: "は全角文字で入力してください" }
+         validates :birthday, presence: true
+         
+
 end
