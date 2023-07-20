5055fffcbd81b4ac278c87916ce7c2b78f7e9807
ぴえん <136320612+shogo-ohya@users.noreply.github.com>
2023-07-20 16:51:09 +0900

    テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
           
          validates :nickname,presence: true
-         validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
-         validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
+         validates :first_name_kana,presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
+         validates :last_name_kana,presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
          validates :first_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: "は全角文字で入力してください" }
          validates :last_name, presence: true, format: { with: /\A[一-龯ぁ-んァ-ン]+\z/, message: "は全角文字で入力してください" }
          validates :birthday, presence: true
---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    gimei = Gimei.name
+    nickname { Faker::Name.first_name }
+    first_name { gimei.first.kanji }
+    last_name { gimei.last.hiragana}
+    first_name_kana { gimei.first.katakana }
+    last_name_kana { gimei.last.katakana }
+  # Gimei
+  # 直接日本語を記入する
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    birthday { Faker::Date.between(from: '1930-01-01', to: '2005-12-31') }
+  end
+end
